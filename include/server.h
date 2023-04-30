017e2b339eee6b6a505002a78d5cac16d924409e
rostekus <rostyslavmosorov@gmail.com>
2023-04-30 22:13:13 +0200

    feat: server struct serve routes

--- include/server.h
+++ include/server.h
@@ -1,11 +1,15 @@
 #ifndef HTTP_SERVER_H
 #define HTTP_SERVER_H
 
+#include "routes.h"
 typedef struct HTTP_Server {
 	int socket;
-	int port;	
+	int port;
+	struct Routes* routes;	
 } HTTP_Server;
 
-void init_server(HTTP_Server* http_server, int port);
+void initServer(HTTP_Server* http_server, int port, struct Routes* routes);
+
+void serveRoutes(HTTP_Server* http_server);
 
 #endif
--- src/server.c
+++ src/server.c
@@ -1,22 +1,119 @@
-#include "HTTP_Server.h"
+#include "server.h"
 #include <netinet/in.h>
 #include <stdio.h>
 #include <sys/socket.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "request.h"
+#include "response.h"
 
-void init_server(HTTP_Server * http_server, int port) {
+void initServer(HTTP_Server * http_server, int port, struct Routes* routes) {
 	http_server->port = port;
+	http_server-> routes = routes;
 
-	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	int server_fd, new_socket;
+	long valread;
+	struct sockaddr_in address;
+	int addrlen = sizeof(address);
 
-	struct sockaddr_in server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(port);
-	server_address.sin_addr.s_addr = INADDR_ANY;
+	// Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
+		perror("In sockets");
+		exit(EXIT_FAILURE);
+	}
 
-	bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(port);
 
-	listen(server_socket, 5);
+	memset(address.sin_zero, '\0', sizeof address.sin_zero);
 
-	http_server->socket = server_socket;
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+	{
+		perror("In bind");
+		exit(EXIT_FAILURE);
+	}
+	if (listen(server_fd, 10) < 0)
+	{
+		perror("In listen");
+		exit(EXIT_FAILURE);
+	}
+
+	http_server->socket = server_fd;
 	printf("HTTP Server Initialized\nPort: %d\n", http_server->port);
+	printf("Routes:\n");
+	displayRoutes(http_server-> routes);
+	printf("\n");
+}
+
+
+void serveRoutes(HTTP_Server* http_server){
+	int client_socket;
+while (1) {
+		char client_msg[4096] = "";
+		printf("Listening on port: %d\n", http_server-> port);
+		client_socket = accept(http_server->socket, NULL, NULL);
+
+		read(client_socket, client_msg, 4095);
+
+		// parsing client socket header to get HTTP method, route
+		char *method = "";
+		char *urlRoute = "";
+
+		char *client_http_header = strtok(client_msg, "\n");
+			
+
+		char *header_token = strtok(client_http_header, " ");
+		
+		int header_parse_counter = 0;
+
+		while (header_token != NULL) {
+
+			switch (header_parse_counter) {
+				case 0:
+					method = header_token;
+				case 1:
+					urlRoute = header_token;
+			}
+			header_token = strtok(NULL, " ");
+			header_parse_counter++;
+		}
+
+		Request* request = initRequest(method, urlRoute);
+		printf("processing request:\n uri: %s\n method: %s\n", request-> uri, request->method);
+		
+				char template[100] = "";
+		
+		if (strstr(urlRoute, "/static/") != NULL) {
+			strcat(template, "static/index.css");
+		}else {
+			struct Route * destination = search(http_server-> routes, urlRoute);
+			strcat(template, "templates/");
+
+			if (destination == NULL) {
+				strcat(template, "404.html");
+			}else {
+				strcat(template, destination->value);
+			}
+		}
+
+		// char * response_data = render_static_file(template);
+
+		int content_length = strlen("");
+		char content_length_str[16];
+		sprintf(content_length_str, "%d", content_length);
+
+		char http_header[4096] = "HTTP/1.1 201 OK\r\n";
+		strcat(http_header, "Content-Length: ");
+		strcat(http_header, content_length_str);
+		strcat(http_header, "\r\n\r\n");
+
+		// Response response = createDefaultResponse();
+
+		send(client_socket, http_header, sizeof(http_header), 0);
+		close(client_socket);
+		// free(response_data);
+}
 }
