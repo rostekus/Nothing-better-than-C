89f4768e06e39beb3946b9575c9448df02801133
rostekus <rostyslavmosorov@gmail.com>
2023-04-30 20:40:49 +0200

    feat: request struct

---
+++ include/response.h
@@ -0,0 +1,9 @@
+struct Response {
+    int status_code;
+    char* status_message;
+    char* content_type;
+    char* body;
+};
+
+struct Response createDefaultResponse();
+char* toHTTPString(struct Response* response);
