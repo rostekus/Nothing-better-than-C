ad40bd7a059c261cf83cc8e23fb407a0c090dbab
rostekus <rostyslavmosorov@gmail.com>
2023-05-02 00:17:16 +0200

    feat: added method attr to request

--- src/request.c
+++ src/request.c
@@ -3,11 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-Request* initRequest(char *method, char *uri){
-    Request*temp = (Request*) malloc(sizeof(Request));
-
-    temp -> method = method;
-    temp -> uri = uri;
-
-    return temp;
+Request* initRequest(char *method, char *uri) {
+    // allocate memory for a Request struct
+    Request *req = malloc(sizeof(Request));
+    
+    // allocate memory for the method string
+    req->method = malloc(strlen(method) + 1);
+    strcpy(req->method, method);
+    
+    // allocate memory for the uri string
+    req->uri = malloc(strlen(uri) + 1);
+    strcpy(req->uri, uri);
+    
+    return req;
 }
